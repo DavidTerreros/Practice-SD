cb04b3eb01edf6a3686dadf50d7101161f8c5681
David Terreros <anthonyterreros21@gmail.com>
2021-05-30 23:19:20 -0500

    Integracion de nuevas Mejores

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const arr = ["Sociedad","1","3","2"]
+
+function nrandom(min, max) {
+    return  Math.floor(Math.random() * (max - min)) + min;
+}
+
 const sayHello = () => {
-    console.log("Hello World!!!")
+    var text1 = document.getElementById("text1");
+    console.log(arr[nrandom(0,arr.length)]);
+    text1.innerHTML = arr[nrandom(0,arr.length)];
 }
